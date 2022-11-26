f6609f9a98060e3a5d948b4739b708b26333597f
arizonv <a.vasgarridoe@gmail.com>
2022-11-26 17:45:08 +0100

    okK

--- registrApp/settings.py
+++ registrApp/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 ###########          c s s     #######################################
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/statics")]
 
 ####################################################################
 ###########          M e d i a     #######################################
