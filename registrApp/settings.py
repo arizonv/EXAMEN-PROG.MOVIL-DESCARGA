c9c2f7e2fc20b78ddae720920c5d49b94962ce39
arizonv <a.vasgarridoe@gmail.com>
2022-11-26 18:07:14 +0100

    okK

--- registrApp/settings.py
+++ registrApp/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+
 ####################################################################
 ###########          M e d i a     #######################################
 
